14826bc24cb163ba553a8a702c05e2a68fe1e6dc
duchieu <duchieu.ctk41@gmail.com>
2021-08-06 12:42:46 +0700

    exercise goroutine in tour golang

---
+++ lec-2/code_fl_example/goroutine/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"./tree"
+	"fmt"
+)
+
+func WalkRecursive(t *tree.Tree, ch chan int) {
+	if t != nil {
+		WalkRecursive(t.Left, ch)
+		ch <- t.Value
+		WalkRecursive(t.Right, ch)
+	}
+}
+
+func Walk(t *tree.Tree, ch chan int) {
+	WalkRecursive(t, ch)
+	close(ch)
+}
+
+func Same(t1, t2 *tree.Tree) bool {
+	ch1, ch2 := make(chan int), make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	for {
+		n1, ok1 := <- ch1
+		n2, ok2 := <- ch2
+		if ok1 != ok2 || n1 != n2 {
+			return false
+		}
+		if !ok1 {
+			break
+		}
+	}
+	return true
+}
+
+func main() {
+	ch := make(chan int)
+	go Walk(tree.New(1), ch)
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
+}
