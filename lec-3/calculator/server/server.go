1ffb37746c875e68d57b773b6a23ed7bb63f996b
duchieu <duchieu.ctk41@gmail.com>
2021-08-07 16:51:50 +0700

    add server calculator

---
+++ lec-3/calculator/server/server.go
@@ -0,0 +1,22 @@
+package server
+
+import (
+	"../calc"
+	"fmt"
+	"net/http"
+)
+
+func RunServer() {
+	fmt.Println("Server is listening in http://localhost:3000")
+	//Defer function will be called process exitst.
+	defer func() {
+		fmt.Println("Server is Stopped")
+	}()
+
+	http.HandleFunc("/calc", calc.Calculator)
+
+	// run server
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		panic("Error when running server")
+	}
+}
