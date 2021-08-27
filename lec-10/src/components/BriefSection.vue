ef4b2e26ba1709a2985532849bc0d15fa35fbd68
hieu hoc code <duchieu.ctk41@gmail.com>
2021-08-27 22:31:42 +0700

    mock data

--- lec-10/src/components/BriefSection.vue
+++ lec-10/src/components/BriefSection.vue
@@ -5,98 +5,42 @@
       <div>
         <h3 class="Brief__Subheading">Brief</h3>
         <div class="Spacer__Wrapper">
-          <p class="Text__Wrapper">
-            Your challenge is to build out this order summary card component and
-            get it looking as close to the design as possible.
+          <p
+            v-for="val in getState.brief"
+            :key="getState.brief[val]"
+            class="Text__Wrapper"
+          >
+            {{ val.content }}
           </p>
-          <p class="Text__Wrapper">
-            You can use any tools you like to help you complete the challenge.
-            So if you've got something you'd like to practice, feel free to give
-            it a go.
-          </p>
-          <p class="Text__Wrapper">Your users should be able to:</p>
+          <!--           
           <ul class="Text__Wrapper">
             <li class="Item__Wrapper">Your users should be able to:</li>
           </ul>
-          <p class="Text__Wrapper">
-            Download the project and go through the README.md file. This will
-            provide further details about the project and help you get set up.
-          </p>
-          <p class="Text__Wrapper">
-            Want some support on the challenge? Join our Slack community and ask
-            questions in the help channel.
-          </p>
+           -->
         </div>
       </div>
       <div>
         <h3 class="Brief__Subheading">Getting started</h3>
         <ol class="OrderedList__List">
-          <li class="OrderedList__ListItem">
+          <li
+            class="OrderedList__ListItem"
+            v-for="val in getState.guide"
+            :key="getState.guide[val]"
+          >
             <div class="Spacer__Wrapper">
-              <p class="Text__Wrapper">Download the starter code</p>
-            </div>
-          </li>
-          <li class="OrderedList__ListItem">
-            <div class="Spacer__Wrapper">
-              <p class="Text__Wrapper">
-                Set up the project with version control (e.g. Git)
-              </p>
-            </div>
-          </li>
-          <li class="OrderedList__ListItem">
-            <div class="Spacer__Wrapper">
-              <p class="Text__Wrapper">
-                Read the README.md file and have a look around the project
-              </p>
-            </div>
-          </li>
-          <li class="OrderedList__ListItem">
-            <div class="Spacer__Wrapper">
-              <p class="Text__Wrapper">
-                Set up the project with version control (e.g. Git)
-              </p>
-            </div>
-            Get colors, fonts etc from the style-guide.md file
-          </li>
-          <li class="OrderedList__ListItem">
-            <div class="Spacer__Wrapper">
-              <p class="Text__Wrapper">
-                Set up your project/file architecture however you want
-              </p>
-            </div>
-          </li>
-          <li class="OrderedList__ListItem">
-            <div class="Spacer__Wrapper">
-              <p class="Text__Wrapper">Start coding!</p>
+              <p class="Text__Wrapper">{{ val.content }}</p>
             </div>
           </li>
         </ol>
         <h3 class="Brief__Subheading">Ideas to test yourself</h3>
         <ul class="BulletedList__List">
-          <li class="BulletedList__ListItem">
+          <li
+            class="BulletedList__ListItem"
+            v-for="val in getState.idea"
+            :key="getState.idea[val]"
+          >
             <div class="Spacer__Wrapper">
-              <p class="Text__Wrapper">
-                Write your styles using a pre-processor, such as Sass, Less or
-                Stylus
-              </p>
-            </div>
-          </li>
-          <li class="BulletedList__ListItem">
-            <div class="Spacer__Wrapper">
-              <p class="Text__Wrapper">
-                Train your eye for detail by getting your solution as close to
-                the design as you can
-              </p>
-            </div>
-          </li>
-          <li class="BulletedList__ListItem">
-            <div class="Spacer__Wrapper">
-              <p class="Text__Wrapper">
-                Try estimating the time it will take for you to build the
-                project. Then see if the time taken matches up to your estimate.
-                Project estimations are a skill that is often overlooked, but is
-                important for professional developers
-              </p>
+              <p class="Text__Wrapper">{{ val.content }}</p>
             </div>
           </li>
         </ul>
@@ -107,8 +51,10 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
   name: "BriefSection",
+  computed: mapGetters({ getState: "allContent" }),
 };
 </script>
 
--- lec-10/src/store/index.js
+++ lec-10/src/store/index.js
@@ -1,8 +1,11 @@
 import { createStore } from "vuex";
+import challenge from "./modules/challenge";
 
 export default createStore({
   state: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    challenge,
+  },
 });
