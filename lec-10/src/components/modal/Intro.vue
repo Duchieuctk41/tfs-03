556c653ba51c953755290532ad345ff54df4ec72
hieu hoc code <duchieu.ctk41@gmail.com>
2021-08-28 20:11:51 +0700

    rename

--- lec-10/src/components/modal/Intro.vue
+++ lec-10/src/components/modal/Intro.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="DemoDetail__Wrapper">
-            <h2 class="Style__HeadingH2">Order Summary</h2>
-            <p class="DemoDetail__Description">
-              You can now listen to millions of songs. audioblocks, and podcasts
-              on any device anywhere you like!
-            </p>
-          </div>
+    <h2 class="Style__HeadingH2">{{state.title}}</h2>
+    <p class="DemoDetail__Description">
+      {{state.description}}
+    </p>
+  </div>
 </template>
 <script>
+import { mapGetters } from "vuex";
 export default {
   name: "Intro",
-}
+  computed: mapGetters({ state: "allModal" }),
+};
 </script>
 <style lang="scss" scoped>
 .DemoDetail__Wrapper {
