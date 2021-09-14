6646feaef51caff0458c2be4e9f0b2b8e0535a2a
duchieu <duchieu.ctk41@gmail.com>
2021-09-14 19:05:24 +0700

    dbclick

--- lec-09/src/views/Home.vue
+++ lec-09/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="home">
+  <div v-if="loading">Loading...</div>
+  <div v-else class="home">
     <section class="todoapp">
       <header class="header">
         <h1>Todo app</h1>
@@ -14,29 +15,117 @@
     </section>
     <section class="main" v-show="todos.length > 0">
       <ul class="todo-list">
-        <li v-for="todo in todos" class="todo" :key="todo.id">
+        <li
+          v-for="todo in filteredTodos"
+          class="todo"
+          :key="todo.id"
+          :class="{
+            completed: todo.completed,
+            editing: editingToDo && todo.id === editingToDo.id,
+          }"
+        >
+          <input
+            v-if="editingToDo"
+            class="edit"
+            type="text"
+            v-model="editingToDo.title"
+            @keyup.enter="doneEdit(todo)"
+            @keyup.esc="cancelEdit"
+          />
           <div class="view">
-            <label>{{ todo.title }}</label>
+            <input
+              class="toggle"
+              type="checkbox"
+              :checked="todo.completed"
+              @change="changeTodoComplete(todo, $event)"
+            />
+            <label @dblclick="editTodo(todo)">{{ todo.title }}</label>
             <button class="destroy" @click="removeTodo(todo)">X</button>
           </div>
         </li>
       </ul>
+      <footer class="footer" v-show="todos.length > 0">
+        <span class="todo-count">
+          <strong>{{ remaining }}</strong>
+          remaining
+        </span>
+        <ul class="filters">
+          <li>
+            <a
+              @click.prevent="visiblity = 'all'"
+              :class="{ selected: visiblity === 'all' }"
+            >
+              all
+            </a>
+          </li>
+          <li>
+            <a
+              @click.prevent="visiblity = 'active'"
+              :class="{ selected: visiblity === 'active' }"
+            >
+              active
+            </a>
+          </li>
+          <li>
+            <a
+              @click.prevent="changeVisibility('completed')"
+              :class="{ selected: visiblity === 'completed' }"
+            >
+              completed
+            </a>
+          </li>
+        </ul>
+      </footer>
     </section>
   </div>
 </template>
 
 <script>
+const filters = {
+  all: todos => {
+    return todos
+  },
+  active: todos => {
+    return todos.filter(todo => {
+      return !todo.completed
+    })
+  },
+  completed: todos => {
+    return todos.filter(todo => {
+      return todo.completed
+    })
+  },
+}
 export default {
   name: 'Home',
-  computed: {
-    todos() {
-      return this.$store.state.todos
-    },
-  },
   data() {
     return {
       newTodo: '',
+      visiblity: 'active',
+      editingToDo: null,
     }
+  },
+  // filters: {
+  //   pluralize: function (n) {
+  //     return n === 1 ? 'item' : 'items'
+  //   },
+  // },
+  created() {
+    this.$store.dispatch('loadTodos')
+  },
+  computed: {
+    loading() {
+      return this.$store.state.loading
+    },
+    todos() {
+      return this.$store.state.todos
+    },
+    filteredTodos() {
+      return filters[this.visiblity](this.todos) // dữ liệu động
+    },
+    remaining() {
+      return filters.active(this.todos).length
+    },
   },
   methods: {
     setNewTodo(e) {
@@ -48,6 +137,35 @@ export default {
     },
     removeTodo(todo) {
       this.$store.dispatch('removeTodo', todo)
+    },
+    changeTodoComplete(todo, e) {
+      this.$store.dispatch('updateTodo', {
+        todo: todo,
+        payload: {
+          completed: e.target.checked,
+        },
+      })
+    },
+    changeVisibility(type) {
+      this.visiblity = type
+    },
+    editTodo(todo) {
+      this.editingToDo = { ...todo }
+    },
+    doneEdit(todo) {
+      this.$store.dispatch('updateTodo', {
+        todo: todo,
+        payload: {
+          title: this.editingToDo.title,
+        },
+      })
+      this.editingToDo = null
+    },
+    cancelEdit() {
+      this.editingToDo = null
+    },
+    changeAll() {
+      this.$store.commit('CHANGE_ALL')
     },
   },
 }
