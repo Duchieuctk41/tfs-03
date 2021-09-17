50d3b3c2fd709e35dc86e9be24deb8ca6ae14207
duchieu <duchieu.ctk41@gmail.com>
2021-09-17 15:48:29 +0700

    learn heap sort

---
+++ lec-13/src/common/api.service.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import API_URL from './config'
+
+const ApiService = {
+  init() {
+    Vue.use(VueAxios, axios)
+    Vue.axios.defaults.baseURL = API_URL
+  },
+  post(resource, params) {
+    return Vue.axios.post(`${resource}`, params)
+  },
+}
+
+export default ApiService
