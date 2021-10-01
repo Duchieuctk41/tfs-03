385b6f9f492996f5e6f9c2859222a346803db1a2
duchieu <duchieu.ctk41@gmail.com>
2021-10-01 22:57:41 +0700

    pagination

---
+++ self-study/pagination_vue/components/Pagination.vue
@@ -0,0 +1,47 @@
+<template>
+  <ul class="ul">
+    <li
+      class="li"
+      v-for="number in pageNumbers"
+      :key="number"
+      @click="$emit('send', number)"
+    >
+      {{ number }}
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  name: 'pagination',
+  emits: ['send'],
+  props: {
+    postsPerPage: Number,
+    totalPosts: Number,
+    totalPage: Number,
+  },
+  data() {
+    return {
+      pageNumbers: [],
+    }
+  },
+  watch: {
+    totalPage: function () {
+      for (let i = 1; i <= this.totalPage; i++) {
+        this.pageNumbers.push(i)
+      }
+    },
+  },
+}
+</script>
+<style scope>
+.ul {
+  list-style-type: none;
+}
+.li {
+  cursor: pointer;
+  padding: 5px;
+  background: #acacac;
+  display: inline-block;
+  margin-right: 5px;
+}
+</style>
---
+++ self-study/pagination_vue/views/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="home">
+    <h1>hello world</h1>
+    <Articles :current-posts="currentPosts" loading />
+    <Pagination
+      :postsPerpage="postsPerPage"
+      :total-posts="posts.length"
+      :total-page="totalPage"
+      @send="receive"
+    />
+  </div>
+</template>
+<script>
+import axios from 'axios'
+import Articles from '../components/Articles.vue'
+import Pagination from '../components/Pagination.vue'
+export default {
+  data() {
+    return {
+      posts: [],
+      loading: false,
+      currentPage: 1,
+      postsPerPage: 10,
+    }
+  },
+  components: {
+    Articles,
+    Pagination,
+  },
+  async beforeCreate() {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    this.posts = res.data
+  },
+  computed: {
+    indexOfLastPost() {
+      return this.currentPage * this.postsPerPage
+    },
+    indexOfFirsPost() {
+      return this.indexOfLastPost - this.postsPerPage
+    },
+    currentPosts() {
+      return this.posts.slice(this.indexOfFirsPost, this.indexOfLastPost)
+    },
+    totalPage() {
+      return Math.ceil(this.posts.length / this.postsPerPage)
+    },
+  },
+  methods: {
+    receive(number) {
+      this.currentPage = number
+    },
+  },
+}
+</script>
