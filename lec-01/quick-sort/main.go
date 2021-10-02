72065b81eb9ba6a6cc1ce9ecb2f8b4d54dc93e10
duchieu <duchieu.ctk41@gmail.com>
2021-10-02 08:38:25 +0700

    quick sort

--- lec-01/quick-sort/main.go
+++ lec-01/quick-sort/main.go
@@ -4,15 +4,14 @@ import "fmt"
 
 func main() {
 	a := []int{3, 2, 1}
-	fmt.Println(quick(a, 0, len(a)-1))
+	fmt.Println(quickSort(a, 0, len(a)-1))
 }
 
-//partition
-func partion(a []int, low, high int) ([]int, int) {
+func partition(a []int, low, high int) ([]int, int) {
 	pivot := a[high]
 	i := low
 	for j := low; j < high; j++ {
-		if a[j] < a[pivot] {
+		if a[j] < pivot {
 			a[i], a[j] = a[j], a[i]
 			i++
 		}
@@ -21,11 +20,11 @@ func partion(a []int, low, high int) ([]int, int) {
 	return a, i
 }
 
-func quick(a []int, low, high int) []int {
+func quickSort(a []int, low, high int) []int {
 	if low < high {
-		a, p := partion(a, low, high)
-		a = quick(a, low, p-1)
-		a = quick(a, p+1, high)
+		a, p := partition(a, low, high)
+		a = quickSort(a, low, p-1)
+		a = quickSort(a, p+1, high)
 	}
 	return a
 }
