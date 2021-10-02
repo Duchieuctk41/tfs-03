7770e59556b9d91858286cfced4e4a7d54a6ebe5
duchieu <duchieu.ctk41@gmail.com>
2021-10-02 08:30:44 +0700

    learn pagination

--- self-study/pagination_vue/views/Home.vue
+++ self-study/pagination_vue/views/Home.vue
@@ -2,18 +2,13 @@
   <div class="home">
     <h1>hello world</h1>
     <Articles :current-posts="currentPosts" loading />
-    <Pagination
-      :postsPerpage="postsPerPage"
-      :total-posts="posts.length"
-      :total-page="totalPage"
-      @send="receive"
-    />
+    <Pagination :total-page="totalPage" @send="receive" />
   </div>
 </template>
 <script>
-import axios from 'axios'
-import Articles from '../components/Articles.vue'
-import Pagination from '../components/Pagination.vue'
+import axios from "axios";
+import Articles from "../components/Articles.vue";
+import Pagination from "../components/Pagination.vue";
 export default {
   data() {
     return {
@@ -21,34 +16,34 @@ export default {
       loading: false,
       currentPage: 1,
       postsPerPage: 10,
-    }
+    };
   },
   components: {
     Articles,
     Pagination,
   },
   async beforeCreate() {
-    const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
-    this.posts = res.data
+    const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    this.posts = res.data;
   },
   computed: {
     indexOfLastPost() {
-      return this.currentPage * this.postsPerPage
+      return this.currentPage * this.postsPerPage;
     },
     indexOfFirsPost() {
-      return this.indexOfLastPost - this.postsPerPage
+      return this.indexOfLastPost - this.postsPerPage;
     },
     currentPosts() {
-      return this.posts.slice(this.indexOfFirsPost, this.indexOfLastPost)
+      return this.posts.slice(this.indexOfFirsPost, this.indexOfLastPost);
     },
     totalPage() {
-      return Math.ceil(this.posts.length / this.postsPerPage)
+      return Math.ceil(this.posts.length / this.postsPerPage);
     },
   },
   methods: {
     receive(number) {
-      this.currentPage = number
+      this.currentPage = number;
     },
   },
-}
+};
 </script>
